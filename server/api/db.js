9bdb875be16441ddf3f1d8b59afebef0eee39866
Felpopinho <felipetomaz2609@gmail.com>
2025-03-22 00:57:03 -0300

    a

--- server/api/db.js
+++ server/api/db.js
@@ -1,11 +1,14 @@
 import admin from 'firebase-admin'
 import 'dotenv/config'
 
+const privateKey64 = process.env.ENCODE_PRIVATE_KEY
+const privateKey = Buffer.from(privateKey64, 'base64').toString('ascii');
+
 const firestoreConfig = {
     type: process.env.TYPE,
     project_id: process.env.PROJECT_ID,
     private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY.replace(/\\\\n/g, '\n'),
+    private_key: privateKey,
     client_email: process.env.CLIENT_EMAIL,
     client_id: process.env.CLIENT_ID,
     auth_uri: process.env.AUTH_URI,
