0269da697d5d8c1a92eb5e904ed796b174d17932
Felpopinho <felipetomaz2609@gmail.com>
2025-04-19 16:07:49 -0300

    All commit

--- server/api/controllers/dnd.js
+++ server/api/controllers/dnd.js
@@ -1,30 +1,62 @@
+import { db } from "../db.js"
+
+export const addCollection = async (req, res) =>{
+    try {
+        const values = {
+            chave: req.body.key,
+            data: req.body.data
+        }
+
+        for (let i = 0; i < values.data.length; i++) {
+            const element = values.data[i];
+            console.log(element)
+            //const docId = element.nome.toLowerCase().replaceAll(" ","-")
+            await db.collection(`${values.chave}`).doc(Object.entries(element)[0][0]).set(element)
+            console.log("documento adicionado")
+        }
+        return res.status(200).json("funcionou")
+        
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+}
+
 export const getClasses = async (req, res) =>{
     try {
-        const path = "../dnd-api/classes.json"
-        await fetch(path).then(data=>{
+        await db.collection("classes").get().then(snpashot =>{
+            const data = snpashot.docs.map(doc =>({
+                ...doc.data(),
+                uid: doc.id
+            }))
             return res.status(200).json(data)
         })
     } catch (error) {
-        return res.status(500).json(error)
+        return res.status(500).json(error.message)
     }
 }
 export const getRacas = async (req, res) =>{
     try {
-        const path = "../dnd-api/racas.json"
-        await fetch(path).then(data=>{
+        await db.collection("racas").get().then(snpashot =>{
+            const data = snpashot.docs.map(doc =>({
+                ...doc.data(),
+                uid: doc.id
+            }))
             return res.status(200).json(data)
         })
     } catch (error) {
-        return res.status(500).json(error)
+        return res.status(500).json(error.message)
     }
 }
 export const getAntecedentes = async (req, res) =>{
     try {
-        const path = "../dnd-api/antecedentes.json"
-        await fetch(path).then(data=>{
+        await db.collection("antecedentes").get().then(snpashot =>{
+            const data = snpashot.docs.map(doc =>({
+                ...doc.data(),
+                uid: doc.id
+            }))
             return res.status(200).json(data)
         })
     } catch (error) {
-        return res.status(500).json(error)
+        return res.status(500).json(error.message)
     }
 }
--- server/api/routes/dnds.js
+++ server/api/routes/dnds.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getClasses, getRacas, getAntecedentes } from '../controllers/dnd.js'
+import { addCollection, getAntecedentes, getClasses, getRacas } from '../controllers/dnd.js'
 
 export const dndRouter = express.Router()
 
-dndRouter.get('/dnd/classes', getClasses)
-dndRouter.get('/dnd/racas', getRacas)
-dndRouter.get('/dnd/antecedentes', getAntecedentes)
+dndRouter.post('/addcollection', addCollection)
+
+dndRouter.get("/dnd/classes", getClasses)
+dndRouter.get("/dnd/classes", getRacas)
+dndRouter.get("/dnd/classes", getAntecedentes)
